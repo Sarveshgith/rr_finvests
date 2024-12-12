d1d61c8ea4550f7a214881d6cbe5f2b98fe0c7be
radha krishnan <radhakrishnanraman2004@gmail.com>
2024-12-12 07:04:09 +0530

    .

--- src/components/Frame8/News.jsx
+++ src/components/Frame8/News.jsx
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './style.css';
 import './script'; 
+import { motion } from "motion/react"
 const News = () => {
   return (
     <>
@@ -12,26 +13,29 @@ const News = () => {
   <main class="main flow news" id='news'>
   <h1 class="headin2 main__heading text-center mt-5 mb-5 ">TOP ECONOMIC NEWS</h1>
   <div class="main__cards cards">
-    <div class="cards__inner">
+    <motion.div initial={{opacity:0, y:200}} transition={{duration:1}} whileInView={{opacity:1, y:0}} viewport={{once: true}} class="cards__inner">
       <div class="cards__card card">
       <div className='news-img'>
             <img src="newsimage1.jpg" alt="" className='news-img1' />
             <p className='news-content1 text-center'>Stocks to watch on December 3: ITC, Pricol, TVS Motor, Swiggy, Torrent Power, Nazara Tech and more</p>
+            <button className="explorebutton">Know more →</button>
         </div>
       </div>
 
       <div class="cards__card card">
       <div className='news-img'><img src="newsimage2.jpg" alt="" className='news-img1' />
         <p className='news-content1 text-center'>Elon Musk’s $56 billion Tesla pay package struck down again, Delaware Judge says board ‘improperly influenced…’</p>
+        <button className="explorebutton">Know more →</button>
         </div>
       </div>
 
       <div class="cards__card card">
       <div className='news-img'><img src="newsimage3.png" alt="" className='news-img1' />
-        <p className='news-content1 text-center'>India's growth may dip below 7% for the first time in four years, economists say</p>
+        <p className='news-content1 text-center'>India's growth may dip below 7% for the first time in four years, economists say </p>
+        <button className="explorebutton">Know more →</button>
         </div>
       </div>
-    </div>
+    </motion.div>
     
     <div class="overlay cards__inner"></div>
   </div>
